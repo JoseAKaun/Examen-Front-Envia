3785647e7733c63cae1e2e2220ddd1c803bb325a
JoseAKaun <joseakaun@me.com>
2024-02-28 13:14:31 -0600

    Table changes

--- src/components/OrdersTable.jsx
+++ src/components/OrdersTable.jsx
@@ -34,27 +34,27 @@ const OrdersTable = () => {
     <div className="container mx-auto p-4">
       <h1 className="text-xl font-bold mb-4">Orders (Examen)</h1>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">Sku</th>
-              <th scope="col" className="px-6 py-3">Name</th>
-              <th scope="col" className="px-6 py-3">Quantity</th>
-              <th scope="col" className="px-6 py-3">Price</th>
-            </tr>
-          </thead>
-          <tbody>
+      <table className="w-full text-sm text-left text-blue-600 bg-blue-500">
+  <thead className="text-xs uppercase bg-green-500 text-white">
+    <tr>
+      <th scope="col" className="px-6 py-3">Sku</th>
+      <th scope="col" className="px-6 py-3">Name</th>
+      <th scope="col" className="px-6 py-3">Quantity</th>
+      <th scope="col" className="px-6 py-3">Price</th>
+    </tr>
+  </thead>
+  <tbody>
             {orders.map((order, orderIndex) =>
             order.items.map((item, itemIndex) => {
               const uniqueKey = `order-${orderIndex}-item-${itemIndex}`;
               const price = parseFloat(item.price);
               return (
-                <tr key={uniqueKey} onClick={() => goToOrderDetail(order.id)} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 hover:bg-gray-200 cursor-pointer">
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.sku}</th>
-                  <td className="px-6 py-4">{item.name}</td>
-                  <td className="px-6 py-4">{item.quantity}</td>
-                  <td className="px-6 py-4">${!isNaN(price) ? price.toFixed(2) : 'Invalid price'}</td>
-                </tr>
+                <tr key={uniqueKey} onClick={() => goToOrderDetail(order.id)} className="odd:bg-blue-100 even:bg-blue-200 hover:bg-yellow-200 cursor-pointer">
+                <th scope="row" className="px-6 py-4 font-medium text-black">{item.sku}</th>
+                <td className="px-6 py-4">{item.name}</td>
+                <td className="px-6 py-4">{item.quantity}</td>
+                <td className="px-6 py-4">${!isNaN(price) ? price.toFixed(2) : 'Invalid price'}</td>
+              </tr>
             );
           })
         )}
